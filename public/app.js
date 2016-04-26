ec831cba9024b178d77f399f60aa34d1085b965c
Alyson <emerald.aly@gmail.com>
2016-04-26 14:17:17 -0400

    switch to ui

--- public/app.js
+++ public/app.js
@@ -1,29 +1,43 @@
-angular.module("ClassApp", ["ngRoute"])
- .config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
-  $routeProvider
-    .when('/about', {
-      templateUrl: "about.html"
-    })
-    .when('/clickgame', {
-      templateUrl: "clickgame.html"
-    })
-    .when('/contact', {
-      templateUrl: "contact.html"
-    })
-    .when('/blog', {
-      templateUrl: "blog.html"
-    })
-    .when('/games', {
-      templateUrl: "games.html"
-    })
-    .when('/projects', {
-      templateUrl: "projects.html"
-    })
-    .when('/rps', {
-      templateUrl: "rps.html"
-    })
-    .otherwise({
-      templateUrl: "initial.html"
-    });
-   $locationProvider.html5Mode(true);
- }]);
+angular.module("PortfolioApp", ["ui.router", "ui.bootstrap"])
+
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/initial');
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'initial.html'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about.html'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'contact.html'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'projects.html'
+      })
+
+
+
+      $locationProvider.html5Mode(true);
+  }]);
+
+ // .config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
+ //  $routeProvider
+ //    .when('/about', {
+ //      templateUrl: "about.html"
+ //    })
+ //    .when('/contact', {
+ //      templateUrl: "contact.html"
+ //    })
+ //    .when('/projects', {
+ //      templateUrl: "projects.html"
+ //    })
+ //    .otherwise({
+ //      templateUrl: "initial.html"
+ //    });
+ //   $locationProvider.html5Mode(true);
+ // }]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var logger= require('morgan');
-var PORT = 8080
+var PORT = process.env.PORT || 8080;
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + "/public"));
