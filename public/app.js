63e73ffe479ab0565203f7e53f82be1dbc761999
Alyson <emerald.aly@gmail.com>
2016-04-26 14:34:46 -0400

    ui router working

--- public/app.js
+++ public/app.js
@@ -1,43 +1,19 @@
-angular.module("PortfolioApp", ["ui.router", "ui.bootstrap"])
-
-.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+angular.module('PortfolioApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/initial');
     $stateProvider
       .state('home', {
         url: '/home',
         templateUrl: 'initial.html'
       })
+
       .state('about', {
         url: '/about',
         templateUrl: 'about.html'
-      })
-      .state('contact', {
-        url: '/contact',
-        templateUrl: 'contact.html'
-      })
-      .state('projects', {
-        url: '/projects',
-        templateUrl: 'projects.html'
+   
       })
 
+    
 
-
-      $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
   }]);
-
- // .config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
- //  $routeProvider
- //    .when('/about', {
- //      templateUrl: "about.html"
- //    })
- //    .when('/contact', {
- //      templateUrl: "contact.html"
- //    })
- //    .when('/projects', {
- //      templateUrl: "projects.html"
- //    })
- //    .otherwise({
- //      templateUrl: "initial.html"
- //    });
- //   $locationProvider.html5Mode(true);
- // }]);
